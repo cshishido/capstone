90421bb011fe71b573dc3898ee533f5387a667e2
cshishido <cshishido@users.noreply.github.com>
2017-09-07 15:53:48 -0700

    added annotations, get feauters changed to handle redundant lines

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,6 +1,8 @@
 from __future__ import division
 from feature_building import count_keyword_123grams, token_pipeline
 from nltk.util import ngrams
+from collections import defaultdict
+import pandas as pd
 
 def keyword_hierarchy(curs, dir_path):
     precedence = {}
@@ -38,10 +40,23 @@ def identify_ingred(ingred_line, precedence_dict, from_url="unknow_url", verbose
 
 def get_recipe_features(recipe_dict, precedence_dict):
     tot = recipe_dict['totalWeight']
-    features = {'label': recipe_dict['label'],
-                'total_wgt': tot}
+    features = defaultdict(float,
+                {'label': recipe_dict['label'].lower(),
+                'total_wgt': tot,
+                'url': recipe_dict['url']})
     for ingred_line in recipe_dict['ingredients']:
         ingred = identify_ingred(ingred_line, precedence_dict, from_url=recipe_dict['url'])
         if ingred:
-            features[ingred[0]] = ingred[1]/tot
+            features[ingred[0]] += ingred[1]/tot
     return features
+
+def get_recipe_df(coll, regx, dir_path='src/'):
+    '''
+    takes a pymongo collection handle and a compiled regular expression ands returns
+    a featurized pandas DataFrame of all recipes in coll matching regx, percent mass
+    by ingredient
+    '''
+    precedence = keyword_hierarchy(coll.find({'label': regx}), dir_path)
+    return pd.DataFrame([get_recipe_features(doc, precedence)
+                        for doc in coll.find({'label': regx})]
+                    ).set_index('url')
--- src/recipe_annotation.py
+++ src/recipe_annotation.py
@@ -1,13 +1,17 @@
 import pandas as pd
+import numpy as np
 
-class recipe(object):
+class Recipe(object):
 
     def __init__(self, recipe_features, key, group=None):
         self.label = recipe_features.pop('label')
         self.total_wgt = recipe_features.pop('total_wgt')
-        self.per_mass = recipe_features
+        self.per_mass = {i: m for i, m in recipe_features.iteritems() if m > 0}
         self.group = group
         self.key = key
+
+    def __repr__(self):
+        return "Recipe({})".format(self.label)
 
     def add_to_group(self, group):
         self.group = group
@@ -16,48 +20,56 @@ class recipe(object):
 
     def get_annotations(self):
         """
-        creates self.annots atribute, string of recipe lines with stats
+        creates self.annots atribute, joinable lines of recipe ingreds
         """
         lines = []
-        for ingred in sorted(self.per_mass, key=(lambda ingred: self.group.stats[ingred]['freq'])):
+        for ingred in sorted(self.per_mass,
+            key=(lambda ingred: self.group.stats[ingred]['freq']),
+            reverse=True):
             stats = self.group.stats[ingred]
-            lines.append("{:16}  {:5.1f}g, used in {:4.1f}%  of recipes".format(
-                ingred, self.per_mass[ingred] * self.total_wgt, stats[0]*100
+            lines.append(
+            "{:16}  |   used in {:3.1f}%  of recipes|".format(
+            ingred.upper() + ": {:4.1f}g".format(self.per_mass[ingred] * self.total_wgt),
+            stats['freq']*100
             ))
-            lines.append("    AVG: {:5.1f}, RANGE: {:5.1f}g - {:5.1f}g,  VARIANCE: {:5.1f}".format(
+            lines.append(
+            "     avg: {:5.1f}g  | range: {:4.1f}g - {:4.1f}g       |  std: {:6.4f}g".format(
                 stats['avg']*self.total_wgt,
                 stats['min']*self.total_wgt,
                 stats['max']*self.total_wgt,
-                stats['var']*self.total_wgt
+                stats['std']*self.total_wgt
             ))
+            lines.append("-"*60)
         self.annots = "\n".join(lines)
 
-class recipe_group(object):
+class RecipeGroup(object):
 
     def __init__(self, member_keys, df):
-        #members is a set of recipe keys
+        #assuming a DataFrame of %mass by ingred, plus label and total_wgt
+        #member_keys are df indices of recipes in group
         self.member_keys = member_keys
         self.df = df
         self.create_members()
         self.get_stats()
 
     def create_members(self):
-        self.members = []
-        for key in member_keys:
-            self.members.append(recipe(self.df.iloc[key].to_dict(),key, self))
-            
+        self.members = {key: Recipe(self.df.loc[key].to_dict(),key, self)
+            for key in self.member_keys}
 
-    def get_stats():
-        #assuming a DataFrame where member keys are idx
-        group_df = self.df.drop('label', axis=1).iloc[list(self.member_keys)]
+    def get_stats(self):
+        group_df = self.df.drop(["label", "total_wgt"], axis=1).loc[list(self.member_keys)]
+        group_df.fillna(0, inplace=True)
+        used_ingreds = (group_df > 0).sum(axis=0) > 0
+        group_df = group_df.loc[:,used_ingreds]
         self.stats = {}
-        for ingred in group_df.colums:
+        for ingred in group_df.columns:
             uses = group_df[ingred] > 0
             amounts = group_df[ingred][uses]
-            ingred_min, ingred_max = amounts.min(), amounts.max()
-            ingred_mean, ingred_var = amounts.mean(), amounts.var()
+            # ingred_min, ingred_max = amounts.min(), amounts.max()
+            # ingred_mean, ingred_var = amounts.mean(), amounts.var()
             self.stats[ingred] = {'freq': uses.mean(),
                                   'min': amounts.min(),
                                   'max': amounts.max(),
                                   'avg': amounts.mean(),
-                                  'var': amounts.var()}
+                                  'var': np.var(np.array(amounts), dtype=np.float64),
+                                  'std': np.var(np.array(amounts), dtype=np.float64)}
